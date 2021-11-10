06a12b31f1a7a8caa333303cd83efacf62d16be3
Shubhankar kumar singh <shubhankars361@gmail.com>
2021-11-10 14:15:06 +0530

    Add emp switch case programme

---
+++ EmpWageSwitchCase/EmpWageSwitchCase/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace EmpWageSwitchCase
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            empSwitch obj1 = new empSwitch();
+            obj1.compute();
+        }
+    }
+}
---
+++ EmpWageSwitchCase/EmpWageSwitchCase/empSwitch.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EmpWageSwitchCase
+{
+    public class empSwitch
+    {
+        public void compute()
+        {
+            const int Present = 1;
+            const int Absent = 0;
+            const int PartTime = 2;
+            const int empWagePerHour = 20;
+            int empHours = 0;
+            int empDailyWage = 0;
+            string empStatus = "";
+            Random rand = new Random();
+            int check = rand.Next(0, 3);
+
+            switch (check)
+            {
+                case Present:
+                    empHours += 8;
+                    empStatus += "Full-Time";
+                    break;
+                case PartTime:
+                    empHours += 4;
+                    empStatus += "Part-Time";
+                    break;
+                case Absent:
+                    empHours += 0;
+                    empStatus += "Absent";
+                    break;
+            }
+            empDailyWage = empHours * empWagePerHour;
+            Console.WriteLine("Employee Wage is " + empDailyWage + " & Employee is " + empStatus);
+        }
+    }
+}
