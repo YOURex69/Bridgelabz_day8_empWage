50025eb51277c6ed915274c284540551a98fae46
Shubhankar kumar singh <shubhankars361@gmail.com>
2021-11-10 14:19:17 +0530

    Add emp Wage OOPs

---
+++ empWageUC7/empWageUC7/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace empWageUC7
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            empWage obj1 = new empWage();
+            obj1.computeWage();
+
+        }
+    }
+}
