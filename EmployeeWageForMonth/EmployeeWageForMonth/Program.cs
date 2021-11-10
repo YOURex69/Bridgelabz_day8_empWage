81e555f049f0de304ffeca57fcbf6c085b07da3f
Shubhankar kumar singh <shubhankars361@gmail.com>
2021-11-10 14:16:42 +0530

    Add programme for emp wage month

---
+++ EmployeeWageForMonth/EmployeeWageForMonth/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace EmployeeWageForMonth
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            empWage obj1 = new empWage();
+            obj1.computeMonthlyWage();
+        }
+    }
+}
---
+++ EmployeeWageForMonth/EmployeeWageForMonth/empWage.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EmployeeWageForMonth
+{
+    class empWage
+    {
+        public void computeMonthlyWage()
+        {
+            const int Present = 1;
+            const int Absent = 0;
+            const int PartTime = 2;
+            const int empWagePerHour = 20;
+            const int empWorkingDays = 20;
+            int empHours = 0;
+            // int empDailyWage = 0;
+            int empMonthlyWage = 0;
+            int countDays = 0;
+            while (countDays < empWorkingDays)
+            {
+                Random rand = new Random();
+                int check = rand.Next(0, 3);
+
+                switch (check)
+                {
+                    case Present:
+                        empHours += 8;
+
+                        break;
+                    case PartTime:
+                        empHours += 4;
+
+                        break;
+                    case Absent:
+                        empHours += 0;
+
+                        break;
+                }
+                countDays++;
+            }
+            empMonthlyWage = empHours * empWagePerHour;
+            Console.WriteLine("Employee Monthly Wage is " + empMonthlyWage);
+        }
+    }
+}
