eee117b6d8693c25562fcfba8a2a2c45ab081e7e
Shubhankar kumar singh <shubhankars361@gmail.com>
2021-11-10 14:17:49 +0530

    Add emp wage condition

---
+++ empWageCondition/empWageCondition/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace empWageCondition
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            empWage obj1 = new empWage();
+            obj1.computeMonthlyWage();
+           
+        }
+    }
+}
