c8ee7dce085bdb8b8696d4d6ec5a84ee24e4bd70
Shubhankar kumar singh <shubhankars361@gmail.com>
2021-11-10 14:12:52 +0530

    Add emp wage Part time programme

---
+++ EmpWagePartTime/EmpWagePartTime/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace EmpWagePartTime
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            empPartTime obj1 = new empPartTime();
+            obj1.compute();
+
+        }
+    }
+}
---
+++ EmpWagePartTime/EmpWagePartTime/empPartTime.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EmpWagePartTime
+{
+    public class empPartTime
+    {
+        public void compute()
+        {
+            const int Present = 1;
+            const int Absent = 0;
+            const int PartTime = 2;
+            const int empWagePerHour = 20;
+            int empHours = 0;
+            int empDailyWage = 0;
+            string empStatus = "";
+            Random rand = new Random();
+            int check = rand.Next(0, 3);
+            if (check == Present)
+            {
+                empHours += 8;
+                empStatus += "Full-Time";
+            }
+            else if (check == PartTime)
+            {
+                empHours += 4;
+                empStatus += "Part-Time";
+            }
+            else
+            {
+                empHours += 0;
+                empStatus += "Absent";
+            }
+            empDailyWage = empHours * empWagePerHour;
+            Console.WriteLine("Employee Wage is " + empDailyWage + " & Employee is " + empStatus);
+        }
+    }
+}
