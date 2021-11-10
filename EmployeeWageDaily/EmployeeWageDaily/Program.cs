f3fe85beec23e7fd3e3079ebf8540ee8eeaedfc0
Shubhankar kumar singh <shubhankars361@gmail.com>
2021-11-10 14:10:56 +0530

    Add Employee Daily Wage Program

---
+++ EmployeeWageDaily/EmployeeWageDaily/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace EmployeeWageDaily
+{
+    class Program
+    {
+        public static void Main(String[] args)
+        {
+            empWageDaily obj1 = new empWageDaily();
+            obj1.compute();
+        }
+        
+    }
+}
---
+++ EmployeeWageDaily/EmployeeWageDaily/empWageDaily.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EmployeeWageDaily
+{
+    public class empWageDaily
+    {
+        public void compute()
+        {
+
+
+            const int Present = 1;
+            const int Absent = 0;
+            const int empWagePerHour = 20;
+            int empHours = 0;
+            int empDailyWage = 0;
+            string empStatus = "";
+            Random rand = new Random();
+            int check = rand.Next(0, 2);
+            if (check == Present)
+            {
+                empHours += 8;
+                empStatus += "Present";
+            }
+            else if (check == Absent)
+            {
+                empHours += 0;
+                empStatus += "Absent";
+            }
+            empDailyWage = empHours * empWagePerHour;
+            Console.WriteLine("Employee Wage is " + empDailyWage + " & Employee is " + empStatus);
+
+
+        }
+    }
+}
