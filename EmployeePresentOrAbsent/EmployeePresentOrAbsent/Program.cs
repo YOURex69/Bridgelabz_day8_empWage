ef07ffa0238a0f23ac15c51a1cea2eafd36363c7
Shubhankar kumar singh <shubhankars361@gmail.com>
2021-11-10 14:08:53 +0530

    Add emp Wage Attendance program

---
+++ EmployeePresentOrAbsent/EmployeePresentOrAbsent/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace EmployeePresentOrAbsent
+{
+    class Program
+    {
+       public static void Main(string[] args)
+        {
+            empAttendance obj1 = new empAttendance();
+            obj1.compute();
+        }
+    }
+}
---
+++ EmployeePresentOrAbsent/EmployeePresentOrAbsent/empAttendance.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EmployeePresentOrAbsent
+{
+    class empAttendance
+    {
+        public void compute()
+        {
+
+            const int Present = 1;
+            const int Absent = 0;
+
+            Random rand = new Random();
+            int check = rand.Next(0, 2);
+            if (check == Present)
+            {
+                Console.WriteLine("Employee is Present");
+            }
+            else if (check == Absent)
+            {
+                Console.WriteLine("Employee is Absent");
+            }
+        }
+    }
+}
